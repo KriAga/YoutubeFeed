65902471e43f605ad0ef5b21f360f46254c60403
Krishna Agarwal <kriaga3@gmail.com>
2021-09-15 03:12:23 +0530

    adding video class

---
+++ video.py
@@ -0,0 +1,6 @@
+class Video:
+    def __init__(self, title, description, pub_datetime, thumbnails):
+        self.title = title
+        self.description = description
+        self.pub_datetime = pub_datetime
+        self.thumbnails = thumbnails
