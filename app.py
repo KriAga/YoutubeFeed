2eb94bb8c73ced1bfde2f1f60bf8c1a918f6071d
Krishna Agarwal <kriaga3@gmail.com>
2021-09-15 03:20:36 +0530

    removing pagination calls from YT api

--- app.py
+++ app.py
@@ -20,8 +20,9 @@ mongo_data = mongo_client['mongo_db_name']['mongo_coll_data']
 
 
 def init_db():
+    # inserting current time as the first published time for all the queries
     docs=[]
-    for query_string in config['find_videos_for']:
+    for query_string in config['find_videos_for']: # Allowing multiple queries to be made
         docs.append({'query_string':query_string, "start_time": int(time.time())})
     mongo_config.insert_many(docs)
 
@@ -40,36 +41,25 @@ def update_data():
         os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
         youtube = build(config['api_service_name'], config['api_version'], developerKey=config['DEVELOPER_KEY'])
 
-        paginating = True
-        next_page_token=''
-        while paginating:
-            # setting up the query parameters from the config file
-            request = youtube.search().list(
-                part="snippet",
-                order=config['order_by'],
-                q=query['query_string'],
-                publishedAfter=datetime.fromtimestamp(int(query['start_time'])).isoformat('T') +'Z',
-                type=config['type'],
-                maxResults=config['max_results'],
-                pageToken=next_page_token
-            )
-            response = request.execute()
+        # setting up the query parameters from the config file
+        request = youtube.search().list(
+            part="snippet",
+            order=config['order_by'],
+            q=query['query_string'],
+            publishedAfter=datetime.fromtimestamp(int(query['start_time'])).isoformat('T') +'Z', # getting epoch from db and converting it to RFC 3339 formatted date-time value
+            type=config['type'],
+            maxResults=config['max_results'] # trying to get as many as results possible in just one call
+        )
+        response = request.execute()
 
-            if response.get('nextPageToken', None):
-                next_page_token, paginating = response.get('nextPageToken', ''), True
-            else:
-                next_page_token, paginating = '', False
-
-            print(next_page_token, paginating)
-            for response_obj in response['items']:
-                video_obj = Video(response_obj['snippet']['title'], 
-                    response_obj['snippet']['description'], 
-                    response_obj['snippet']['publishTime'], 
-                    response_obj['snippet']['thumbnails'])
-                # print(video_obj.title)
-                insert_db()
-
-        print('pagination part done')
+        # iterating on all the item/video results
+        for response_obj in response['items']:
+            video_obj = Video(response_obj['snippet']['title'], 
+                response_obj['snippet']['description'], 
+                response_obj['snippet']['publishTime'], 
+                response_obj['snippet']['thumbnails'])
+            # print(video_obj.title)
+            insert_db()
 
         mongo_config.update({'query_string':query['query_string']}, {"$set":{'start_time':int(query['start_time'])+10}})
 
@@ -89,6 +79,5 @@ def home():
 
 
 if __name__ == "__main__":
-    
-    init_db()
+    init_db() # initialising the db with the query params and the start_time 
     app.run()
