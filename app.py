70a8a11a13e52d8ec726a16de1272630b0f3a366
Krishna Agarwal <kriaga3@gmail.com>
2021-09-17 02:52:07 +0530

    insertion and fetching from database at endpoint

--- app.py
+++ app.py
@@ -1,44 +1,46 @@
 import os
 import json
+import time
+import re
+
 import googleapiclient.discovery
 import googleapiclient.errors
 import apiclient
 from googleapiclient.discovery import build
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Flask
+
+from datetime import datetime, timedelta
+from flask import Flask, jsonify, request
+
 import pymongo
 from pymongo import UpdateOne
+
 from video import Video
-import time
-from datetime import datetime, timedelta
+
 
 # config file that stores all the search_term paramters and api keys
 config = json.load(open('config.json'))
 
 # connecting to mongodb database
 mongo_client = pymongo.MongoClient(config['mongodb_url'])
-mongo_config = mongo_client['mongo_db_name']['mongo_coll_config']
-mongo_data = mongo_client['mongo_db_name']['mongo_coll_data']
+mongo_config = mongo_client[config['mongo_db_name']][config['mongo_coll_config']]
+mongo_data = mongo_client[config['mongo_db_name']][config['mongo_coll_data']]
 
-
+# initialising the configuration details of the search queries in the db
 def init_db():
-    # inserting current time as the first published time for all the queries
+    mongo_config.drop()
+    mongo_data.drop()
     docs=[]
     for query_string in config['find_videos_for']: # Allowing multiple queries to be made
+        # inserting current time as the first published_time/start_time for all the queries
         docs.append({'_id':query_string, "start_time": int(time.time())})
-    upserts=[ UpdateOne({'_id':doc['_id']}, {'$setOnInsert':doc}, upsert=True) for doc in docs]
+    # creating bulk updates
+    upserts=[ UpdateOne({'_id':doc['_id']}, {'$set':doc}, upsert=True) for doc in docs]
     mongo_config.bulk_write(upserts)
-
-
-def insert_db(video_details):
-    print("inserted", video_details['title'])
-    pass
-
-
+    
 # function that is going to update the db with the video details
 def update_data():
-    print('update_data')
-
+    searching on all the 
     for search_term in mongo_config.find():
         key_worked = False # Flag to track if the key worked or not
         for key in config['DEVELOPER_KEY']:
@@ -46,7 +48,7 @@ def update_data():
             try:
                 # setting up the connection with youtube api
                 youtube = build(config['api_service_name'], config['api_version'], developerKey=key)
-                # print(search_term['_id'], datetime.fromtimestamp(int(search_term['start_time'])).isoformat('T') +'Z')
+                print(search_term['_id'], datetime.fromtimestamp(int(search_term['start_time'])).isoformat('T') +'Z')
                 # setting up the query parameters from the config file
                 request = youtube.search().list(
                     part="snippet",
@@ -64,21 +66,38 @@ def update_data():
 
             if key_worked:
                 # iterating on all the item/video results
+                videos_arr = []
                 for response_obj in response['items']:
-                    video_obj = Video(response_obj['snippet']['title'], 
+                    utc_dt = datetime.strptime(response_obj['snippet']['publishTime'], '%Y-%m-%dT%H:%M:%SZ')
+                    published_datetime_epoch = int((utc_dt - datetime(1970, 1, 1)).total_seconds())
+                    video_obj = Video(response_obj['id']['videoId'],
+                        response_obj['snippet']['title'], 
                         response_obj['snippet']['description'], 
-                        response_obj['snippet']['publishTime'], 
-                        response_obj['snippet']['thumbnails'])
+                        published_datetime_epoch, 
+                        response_obj['snippet']['thumbnails'],
+                        response_obj['snippet']['channelTitle'])
                     # print(video_obj.title)
-                    insert_db(video_obj.__dict__)
+                    video_details = video_obj.__dict__
+                    video_details['_id'] = video_details['video_id']
+                    video_details['query_string'] = search_term['_id']
+                    video_details['updated_time'] = datetime.now().strftime("%Y%m%d_%H%M%S")
+                    videos_arr.append(video_details)
+                    print("inserted", video_details['video_id'], video_details['pub_datetime'],video_details['updated_time'])
+                upserts = [ UpdateOne({'_id':video_detail['_id']}, {'$set':video_detail}, upsert=True) for video_detail in videos_arr]
+                mongo_data.bulk_write(upserts)
 
+                if 'title_description' not in mongo_data.index_information().keys():
+                    # Create text index on title and description fields
+                    mongo_data.create_index([('title', 'text'), ('description', 'text')], weights={'title':2, 'description':1},
+                            name='title_description')
+                
 
-        mongo_config.update_one({'_id':search_term['_id']}, {"$set":{'start_time':int(search_term['start_time'])+10}})
-
+        mongo_config.update_one({'_id':search_term['_id']}, {"$set":{'start_time':int(search_term['start_time'])+config['update_interval']}})
+        
 
 # Schdeuler code to schedule the updation of the db every 10 seconds
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(update_data,'interval', seconds=10)
+sched.add_job(update_data,'interval', seconds=config['update_interval'])
 sched.start()
 
 app = Flask(__name__)
@@ -88,6 +107,22 @@ app = Flask(__name__)
 def home():
     """ Function for test purposes. """
     return "Welcome Home :) !"
+
+@app.route("/videos")
+def videos():
+    search_query = request.args.get("search_query", None)
+    videos_per_page = request.args.get("videos_per_page", 10)
+    page_number = request.args.get("page_number", 0)
+    
+    selection_query = {"$or":[
+        {"title": {'$regex' : f'.*{search_query}.*', '$options':'$i'}},
+        {"description": {'$regex' : f'.*{search_query}.*', '$options':'$i'}}
+    ]} if search_query else {} 
+    total_videos =  mongo_data.find(selection_query).count() if search_query else mongo_data.count()
+    data = list(mongo_data.find(selection_query).sort('pub_datetime', -1).skip(page_number*videos_per_page).limit(videos_per_page))
+    
+    response = {'total_videos': total_videos, 'current_page': page_number, 'videos_per_page': videos_per_page, 'data':data}
+    return jsonify(response)
 
 
 if __name__ == "__main__":
