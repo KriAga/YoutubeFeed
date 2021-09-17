d8ce5eb43718b0c0dfd3ad6b5a56f9be8cbc9589
Krishna Agarwal <kriaga3@gmail.com>
2021-09-17 15:27:05 +0530

    adding mongo client connection to utilities

--- youtube_app/utility.py
+++ youtube_app/utility.py
@@ -1,4 +1,17 @@
+import pymongo
+import pymongo.errors
+from config import mongo_url
 import logging
+
+
+def get_mongo_client():
+    # connecting to mongodb database
+    try:
+        mongo_client = pymongo.MongoClient(mongo_url)
+        return mongo_client
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        raise err
+
 
 def get_logger():
     # Creating file logger
