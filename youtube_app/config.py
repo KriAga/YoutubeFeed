ee5d34d7c106b77c9e74f5f8bdce45d542df3037
Krishna Agarwal <kriaga3@gmail.com>
2021-09-18 00:09:25 +0530

    running locally

--- youtube_app/config.py
+++ youtube_app/config.py
@@ -4,6 +4,6 @@ import os
 # config file that stores all the search_term paramters and api keys
 config = json.load(open('config.json'))
 # picking up the mongo config values from the system environment for docker
-mongo_url = "mongodb://{host}:{port}/".format(host=os.environ.get("MONGO_HOST"), port=os.environ.get("MONGO_PORT"))
+# mongo_url = "mongodb://{host}:{port}/".format(host=os.environ.get("MONGO_HOST"), port=os.environ.get("MONGO_PORT"))
 # To be utilised when running locally 
-# mongo_url = "mongodb://{host}:{port}/".format(host="0.0.0.0", port="27017")
+mongo_url = "mongodb://{host}:{port}/".format(host="127.0.0.1", port="27017")
